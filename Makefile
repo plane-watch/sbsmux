759a20868342fe319d98b2a131f65993e55e8477
Mike Nye <mike.nye@gmail.com>
2024-02-18 22:09:06 +0800

    add end to end testing / make test

--- Makefile
+++ Makefile
@@ -13,6 +13,10 @@ install_modules:
 mkbindir:
 	@mkdir -p ./bin
 
+test:
+	go test -v -failfast -count=2 -timeout=5m ./...
+	bash ./test.sh
+
 clean:
 	@rm -v ./bin/sbsmux > /dev/null 2>&1 || true
 	@rmdir -v ./bin > /dev/null 2>&1 || true
---
+++ test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+TMPFILE=$(mktemp)
+
+make sbsmux
+
+./bin/sbsmux \
+  --statsinterval=5 \
+  --bufsizein=100000 \
+  --bufsizeout=100000 \
+  &
+SBSMUX_PID=$!
+
+echo "wait for sbsmux to start up"
+sleep 5
+
+nc 127.0.0.1 30003 > "$TMPFILE" &
+NC_OUT_PID=$!
+
+cat "./cmd/sbsmux/test_data/readsb.sbs.output" | pv --name " SBS data sent " -b -L 256000 | nc -v 127.0.0.1 30103
+
+echo "wait for sbsmux to flush all data"
+sleep 10
+
+kill $NC_OUT_PID > /dev/null 2>&1
+kill $SBSMUX_PID > /dev/null 2>&1
+
+
+diff --strip-trailing-cr <(sort "./cmd/sbsmux/test_data/readsb.sbs.output") <(sort "$TMPFILE")
+EXITCODE=$?
+
+rm "$TMPFILE"
+
+if [[ "$EXITCODE" -ne 0 ]]; then
+    echo "TEST FAILS - OUTPUT DIFFERS FROM INPUT"
+else
+    echo "TEST SUCCEEDS - OUTPUT MATCHES INPUT"
+fi
+
+exit $EXITCODE
